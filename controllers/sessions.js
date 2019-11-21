192bcc0b9749f51438c96f27a567436e4aa1283a
punitha86 <punith.job@gmail.com>
2019-11-21 13:41:58 -0800

    getting boards based on user

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -2,25 +2,39 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 const User = require('../models/users.js');
-
+const Boards = require('../models/boards.js');
 router.post('/', (req, res) => {
-	console.log('Entered POST route for sessions');
-  User.findOne({username:req.body.username}, (err, foundUser) => {
-		console.log('Entered User.findOne function');
-    if(foundUser === null){
-			console.log('Unable to find a user');
-      res.json({message:'user not found', status:401});
+  console.log('Entered POST route for sessions');
+  User.findOne({
+    username: req.body.username
+  }, (err, foundUser) => {
+    console.log('Entered User.findOne function');
+    if (foundUser === null) {
+      console.log('Unable to find a user');
+      res.json({
+        message: 'user not found',
+        status: 401
+      });
     } else {
-			console.log('Found a user. Checking password.');
+      console.log('Found a user. Checking password.');
       const doesPasswordMatch = bcrypt.compareSync(req.body.password, foundUser.password);
       if (doesPasswordMatch) {
-				console.log('Match. Assigning req.session.user to found user.');
+        console.log('Match. Assigning req.session.user to found user.');
         req.session.user = foundUser;
-        res.json(foundUser)
+				console.log("userId in sessions",req.session.user.id);
+        //res.json(foundUser);
+        ///getting the boards tied with the user////////
+        //foundUser="5dd6df85bc92ba84d7317bb3"
+        Boards.find({
+          assignedTo: req.session.user.id
+        }, (err, userBoards) => {
+          console.log(userBoards);
+          res.json(userBoards);
+        })
       } else {
-				console.log('error!');
+        console.log('error!');
         res.json({
-          message:'failed login'
+          message: 'failed login'
         });
       }
     }
@@ -28,16 +42,16 @@ router.post('/', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-	console.log('Entering GET route for sessions');
+  console.log('Entering GET route for sessions');
   res.json(req.session.user);
 });
 
 router.delete('/', (req, res) => {
-	console.log('Entering DELETE route for sessions');
+  console.log('Entering DELETE route for sessions');
   req.session.destroy(() => {
-		console.log('Session destroyed');
+    console.log('Session destroyed');
     res.json({
-      destroyed:true
+      destroyed: true
     });
   });
 });
