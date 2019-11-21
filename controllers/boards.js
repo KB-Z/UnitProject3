535b7407c9271e50030deefd63df9b5f321d0e7e
punitha86 <punith.job@gmail.com>
2019-11-21 11:23:04 -0800

    boards post and put working at this point

--- controllers/boards.js
+++ controllers/boards.js
@@ -2,22 +2,35 @@ const express = require('express');
 const router = express.Router();
 const Boards = require('../models/boards.js');
 const User = require('../models/users.js');
-///getting all the projects for showing
+///getting all the boards for refernce
+router.get('/', (req, res) => {
+  // console.log(req);
+Boards.find({},(err,allboards) => {
+  res.json(allboards);
+})
+})
+//getting boards based on the user logged in
 router.get('/:userid', (req, res) => {
   // console.log(req);
 let x ; // res.json('hi');
   User.findById(req.params.userid, (err,foundUser) => {
   console.log(foundUser.boards);
+  res.json(foundUser.boards);
   });
-res.json('afsf');
-
 })
 
-////to create new projects
+////to create new boards
 router.post('/', (req, res) => {
   Boards.create(req.body, (error, createdBoard) => {
       res.json(createdBoard);
   });
-})
+});
+
+//update the boardSchema
+router.put('/update/:id', (req, res) => {
+    Boards.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedBoard) => {
+        res.json(updatedBoard);
+    })
+});
 
 module.exports = router;
