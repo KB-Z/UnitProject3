7fd205438ff8e258236f0496b5f60e10e9adee17
Joseph Pena <jp1535@outlook.com>
2019-11-21 12:52:44 -0600

    modified controllers/boards.js with several console.logs for debugging purposes

--- controllers/boards.js
+++ controllers/boards.js
@@ -2,22 +2,26 @@ const express = require('express');
 const router = express.Router();
 const Boards = require('../models/boards.js');
 const User = require('../models/users.js');
+
 ///getting all the projects for showing
 router.get('/:userid', (req, res) => {
+	console.log('Entering GET route for boards with user id');
   // console.log(req);
-let x ; // res.json('hi');
+	let x ; // res.json('hi');
   User.findById(req.params.userid, (err,foundUser) => {
-  console.log(foundUser.boards);
+		console.log('Found user!');
+  	console.log(foundUser.boards);
+		res.json('afsf');
   });
-res.json('afsf');
-
-})
+});
 
 ////to create new projects
 router.post('/', (req, res) => {
-  Boards.create(req.body, (error, createdBoard) => {
-      res.json(createdBoard);
+  console.log('Entered POST route for boards');
+	Boards.create(req.body, (error, createdBoard) => {
+      console.log('Board created!');
+			res.json(createdBoard);
   });
-})
+});
 
 module.exports = router;
